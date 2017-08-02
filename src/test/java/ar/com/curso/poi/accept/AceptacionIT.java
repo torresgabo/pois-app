4d9d2c5f4ab713ca54822efe3d02d00fdcf90b0e
Unknown <torresgabo@gmail.com>
2017-08-02 12:03:43 -0500

    Cambia de lugar el test de aceptacion

--- src/test/java/ar/com/curso/poi/accept/AceptacionIT.java
+++ src/test/java/ar/com/curso/poi/accept/AceptacionIT.java
@@ -1,8 +1,9 @@
-package ar.com.curso.poi.test;
+package ar.com.curso.poi.accept;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -10,27 +11,21 @@ import org.junit.Test;
 
 import ar.com.curso.poi.controladores.ControladorHome;
 
-public class AceptacionTest {
+public class AceptacionIT {
 	ControladorHome controlador = new ControladorHome();
 
 	@Test
 	public void validaElPoiMasCercano() throws Exception {
-		// Obelisco
-		// lon -34.6037389
-		// lat -58.3837644
 		String url = "http://localhost:8080/pois-app/Buenos_Aires_Turismo/retornapoicercano/-34.6037389/-58.3837644";
-
 		String data = llamarUrl(url);
-
 		assertThat(data).contains("Cuartetas");
 	}
 
 	public String llamarUrl(String url) throws Exception {
-
 		URL urlConnect = new URL(url);
 		URLConnection connection = urlConnect.openConnection();
-		DataInputStream inStream = new DataInputStream(connection.getInputStream());
-		return inStream.readLine();
+		connection.connect();
+		return new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
 	}
 
 }
